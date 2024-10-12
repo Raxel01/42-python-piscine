72c7752583ea8bac8603af998fac1eb7fe139d8a
ABDELALI AIT TALB <aittalbabdelalie@gmail.com>
2024-10-12 18:23:14 +0100

    fix Day01/(ex01): adapt fraction of seconds and scientific notation to subject .4f and .2e

--- Day00/ex01/format_ft_time.py
+++ Day00/ex01/format_ft_time.py
@@ -3,18 +3,21 @@ import time
 _time_now_ = datetime.datetime.now()
 # year| moth| day | hour | minute | second | microsecond
 
-_seconds_ = time.time()
-_second_scientific_notation = "{:e}".format(_seconds_)
+_seconds_origine_ =time.time()
+
+_seconds_formated = f"{_seconds_origine_:,.4f}"
+
+_second_scientific_notation = f'{_seconds_origine_:.2e}'
 
 
 _month_ = _time_now_.strftime("%b")
 _year_  = _time_now_.year
 _day_   = _time_now_.day
-_first_part_ = f"Seconds since January 1, 1970: {_seconds_} or {_second_scientific_notation} in scientific notation"
+
+_first_part_ = f"Seconds since January 1, 1970: {_seconds_formated} or {_second_scientific_notation} in scientific notation"
 _second_part_ = f"{_month_} {_day_} {_year_}"
 print(_first_part_)
 print(_second_part_)
 
-# transform seonds to readable format using ctime()
-
+# transform secnds to readable format using ctime()
 # format date objects to readable strings
