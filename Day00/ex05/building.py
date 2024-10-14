89252fdc76f22ec01b85b751ba221302321885cb
ABDELALI AIT TALB <aittalbabdelalie@gmail.com>
2024-10-14 18:52:13 +0100

    Feat(Day00/ex05): Finish ex05 just recheck for ctrl+D behaviour

--- Day00/ex05/building.py
+++ Day00/ex05/building.py
@@ -1,4 +1,6 @@
 import sys
+from string import ascii_lowercase, ascii_uppercase
+from string import digits, whitespace, punctuation
 
 
 def get_input() -> str:
@@ -10,6 +12,7 @@ def get_input() -> str:
     _user_input = ''
     while 1:
         line = input('what\'s the text to count : \n')
+        print('Your line : |', line, "|")
         if line:
             _user_input = line
             break
@@ -22,28 +25,50 @@ def global_Validator(argv) -> str:
      + else either there is no args else there is more args > 2, 3 ,4,5!
      + so return the  string : given one or readed from input !
     """
-    _data_to_process_ = ''
+    _user_input = ''
     match len(argv):
         case 2:
             match len(argv[1]):
                 case 0:
-                    _data_to_process_ = get_input()
+                    _user_input = get_input()
                 case _:
-                    _data_to_process_ = argv[1]
+                    _user_input = argv[1]
             # two cases maybe string have varacteres or "\0"
         case 1:
-            _data_to_process_ = get_input()
+            _user_input = get_input()
         case _:
             raise AssertionError('AssertionError: Sequence args provided !')
-    return _data_to_process_
+    return _user_input
 
 
-def type_recognizer(input: str):
+def type_recognizer(_user_input: str):
     """ + this function is maked to count
         + all occurent of every caractere type :
-        + ['upper, lower', 'punct', 'digits',  'spaces']
+        + [lower' ,'upper' , 'punct', 'digits',  'spaces']
     """
-    pass
+    print(f'The text contain {len(_user_input)}')
+    # declaration
+    upper, lower, puncts, Digits, spaces = 0, 0, 0, 0, 0
+
+    for char in _user_input:
+        if char in ascii_lowercase:
+            lower += 1
+        elif char in ascii_uppercase:
+            upper += 1
+        elif char in punctuation:
+            puncts += 1
+        elif char in digits:
+            Digits += 1
+        elif char in whitespace:
+            spaces += 1
+    counter_dic = {
+        'upper': f'{upper} upper letters',
+        'lower': f'{lower} lower letters',
+        'puncts': f'{puncts} punctuation marks',
+        'spaces': f'{spaces} spaces',
+        'Digits': f'{Digits} digits'
+    }
+    return counter_dic
 
 
 def main():
@@ -51,8 +76,10 @@ def main():
        + general steps that my program does so big
        + functionnalities will be called Here
     """
-    provided_string = global_Validator(sys.argv)
-    print(f'The text contain {len(provided_string)}') 
+    _user_input = global_Validator(sys.argv)
+    counter_dict = type_recognizer(_user_input)
+    for key, value in counter_dict.items():
+        print(f'{key} {value}')
 
 
 if __name__ == '__main__':
